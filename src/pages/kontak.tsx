98c4ba2c98147d65a9651c071f395173187b4e99
Febriansyah <febriansyahid@outlook.com>
2023-04-04 13:51:53 +0700

    kontak

--- src/pages/kontak.tsx
+++ src/pages/kontak.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { NextSeo } from "next-seo";
 
-export default function NotFound() {
+export default function Kontak() {
     return (
         <Center>
             <NextSeo
